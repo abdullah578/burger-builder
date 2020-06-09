c298ef7f8ee759ab0a26d280a5d30cff0a88282f
abdullah578 <abdullah.am2000@gmail.com>
2020-06-09 00:18:09 -0400

    made some styling changes and handled some warnings

--- src/Components/Burger/Burger.js
+++ src/Components/Burger/Burger.js
@@ -6,7 +6,10 @@ const burger = (props) => {
   let ingredients = Object.keys(props.ingredients)
     .map((curr, index1) => {
       return [...new Array(props.ingredients[curr])].map((c, index2) => (
-        <BurgerIngredient key={(index1 + 1) * (index2 + 1)} type={curr} />
+        <BurgerIngredient
+          key={index1.toString() + index2.toString()}
+          type={curr}
+        />
       ));
     })
     .reduce((arr, elem) => arr.concat(elem), []);
